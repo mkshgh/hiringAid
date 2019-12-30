609a108200d7d919d84b3fbbcd4e7ad0d32edcde
mkshgh <mistenigma.mg@gmail.com>
2019-12-30 21:58:13 +0545

    main file

---
+++ index.py
@@ -0,0 +1,20 @@
+# Get the file
+# Analyze for grammar mistakes __points and __corrections
+
+
+# Convert the resume file into a series of sentences
+# @todo create a ratio of __average_word_count and __average_sentence lets say 20
+# @todo __sentence_count, __word_count, __paragraph_count
+
+
+# Create some measures for the evaluation of the sentences
+# Basic Information:
+# @todo __education, __experiences = {filed_i, duration_i, time_before_last_job}, 
+# Additional Information:
+# @competences teamwork, communication, analytical, result_driven, leadership, etc
+
+# Extract MEASURABLE_RESULTS from the sentences as said on above
+
+# Comparing the measures and data to assign the points.
+
+# Display the data in the visual way that shows the users some insight to the data.
